a4781d5f3bc0740c1559b84f8716fb83d025d427
Ian Ross <ianross.codes@gmail.com>
2024-11-01 17:14:32 -0600

    Add dependencies

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { View, StyleSheet } from 'react-native';
 import AppStack from './src/screens/AppStack/AppStack';
+import { NavigationContainer } from '@react-navigation/native';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <AppStack />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <AppStack />
+      </View>
+    </NavigationContainer>
   );
 }
 
