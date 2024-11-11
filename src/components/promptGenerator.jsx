9e3923967fd4c0e76afa4620418c85b4170aa12c
Ian Ross <ianross.codes@gmail.com>
2024-11-10 17:59:09 -0700

    Add finalized tool page design

--- src/components/promptGenerator.jsx
+++ src/components/promptGenerator.jsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 import Backdrop from './../assets/images/purple_victorian_background.jpg'
 import LaceBackdrop from './../assets/images/white-lace-backdrop.png'
 import LaceBorder from './../assets/images/white-lace-border.png'
+import LaceButton from './../assets/images/white-lace-button.png'
 import { useFonts } from 'expo-font';
 
 export default function PromptGenerator({  }) {
@@ -35,37 +36,37 @@ export default function PromptGenerator({  }) {
       <View style={{ alignItems: 'center', height: "100%" }}>
         <ImageBackground style={{flex: 1, alignItems: "center", width: "100%", height: "100%"}} source={Backdrop}>
 
-        <View style={{display: "flex", alignItems: "center", marginTop: 20}}>
+        <View style={{display: "flex", alignItems: "center", marginTop: 15}}>
           <Text style={{ fontSize: 36, color: "white", fontFamily: "OswaldVariable"}}>Prompt Generator</Text>
           <Text style={{ fontSize: 18, color: "white", fontFamily: "OswaldVariable"}}>How many prompts would you like to generate?</Text>
         </View>
-        <View style={{marginTop: 25, alignItems: "center"}}>
-          <Image style={{position: "absolute", height: 115, width: 115, bottom: 50}}source={LaceBackdrop}/>          
+        <View style={{marginTop: 20, alignItems: "center"}}>
+          <Image style={{position: "absolute", height: 115, width: 115, bottom: 68}}source={LaceBackdrop}/>          
           <InputSpinner
             width={300}
             max={10}
             min={1}
             step={1}
             fontFamily={"AncientMedium"}
-            color={"#4B0082"}
-            colorMax={"#3C1361"}
-            colorMin={"#6A0DAD"}
+            color={"#130124"}
             fontSize={45}
             value={numberOfPrompts}
             onChange={(num) => {
              setNumberOfPrompts(num)
             }}
+            style={{marginBottom: 8}}
           />
-          
-          <View style={{marginTop: 35}}>
-            <TouchableOpacity onPress={() => { handleSubmit() }} style={{  borderWidth: 1, borderColor: "white", borderRadius: 30, backgroundColor: "black", padding: 10, width: 300, alignItems: "center" }}>
-              <Text style={{color: "white", fontSize: 18, fontFamily: "OswaldVariable"}}>Submit</Text>
-            </TouchableOpacity>  
+
+          <View style={{marginTop: 25, display: "flex", alignItems: "center"}}>
+            <Image onPress={() => { handleSubmit() }} style={{height: 90, width: 220, position: "absolute", zIndex: 1, bottom: -10, resizeMode: "stretch" }}source={LaceButton} />
+            <TouchableOpacity onPress={() => { handleSubmit() }} style={{  borderWidth: 1, borderColor: "white", backgroundColor: "black", height: 70, paddingVertical: 15, width: 180, alignItems: "center" }}>
+              <Text style={{color: "white", fontSize: 20, fontFamily: "OswaldVariable"}}>Submit</Text>
+            </TouchableOpacity>
           </View>
         </View>
         <View style={{display: "flex", alignItems: "center"}}>
           <Image style={{height: 450, width: 450, position: "absolute", zIndex: 10, marginLeft: -25, marginTop: -10}}source={LaceBorder} />
-          <View style={{height: 375, width: 375, backgroundColor: "black", position: 'relative', marginTop: 15}}>
+          <View style={{height: 375, width: 375, backgroundColor: "black", position: 'relative', marginTop: 25}}>
             <View style={{padding: 20}}>
               <ScrollView style={{height: "100%", zIndex: 10}}>
                <Text style={{fontFamily: "OswaldVariable", fontWeight: 500, lineHeight: 24, fontSize: 18, textAlign: "center", color: "white", padding: 25}}>{response}</Text>
@@ -73,8 +74,8 @@ export default function PromptGenerator({  }) {
             </View>
           </View>
         </View>
-        <View style={{width: 40, height: 16, bottom: 103, left: 92, position: "absolute", backgroundColor: "black", borderWidth: 1, borderColor: "white", transform: [{ rotate: '146deg' }]}}></View>
-        <Image style={{height: 110, width: 80, borderRadius: 45, position: "absolute", bottom: 5, left: 20, borderWidth: 1, borderColor: "white"}}source={require('./../assets/images/EdgarAllan.webp')} />
+        <View style={{width: 45, height: 10, bottom: 107, left: 92, position: "absolute", backgroundColor: "black", borderWidth: 2, borderColor: "white", zIndex: 2, transform: [{ rotate: '146deg' }]}}></View>
+        <Image style={{height: 110, width: 80, borderRadius: 45, position: "absolute", bottom: 6, left: 20, borderWidth: 1, borderColor: "white"}}source={require('./../assets/images/EdgarAllan.webp')} />
         </ImageBackground>
       </View>
     );
